5996bc5872e446654fa29c58e98eda5a4577e525
stjulians-godot <pedroso.ruben@gmail.com>
2026-02-26 17:16:32 +0000

    ggdxcuivgftduighidtuogh2rurghui

--- prefabs/bullet.gd
+++ prefabs/bullet.gd
@@ -1,24 +1,16 @@
-extends CharacterBody2D
+extends Area2D
 
+var speed: float = 600.0
+var direction: Vector2 = Vector2.RIGHT
 
-const SPEED = 300.0
+func _physics_process(delta):
+	# Move the bullet
+	position += direction * speed * delta
 
-
-func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
-	move_and_slide()
+func _on_body_entered(body):
+	# Hit something! Destroy the bullet
+	queue_free()
+	
+	# If we hit an enemy, damage it
+	if body.has_method("take_damage"):
+		body.take_damage(1)
